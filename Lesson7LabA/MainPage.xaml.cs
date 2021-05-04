082876f267d4f5ba9d0d4b632521e71cfcfe5386
tylerg05 <tylergardella05@gmail.com>
2021-05-03 19:28:30 -0500

    Add project files.

---
+++ Lesson7LabA/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lesson7LabA
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnSelection(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+            {
+                return;
+            }
+            TylerFactData fact = (TylerFactData)e.SelectedItem;
+            DisplayAlert("The Fact", fact.TheFact, "Ok");
+        }
+    }
+}
---
+++ Lesson7LabA/TylerFactData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson7LabA
+{
+    class TylerFactData
+    {
+        public TylerFactData()
+        {
+
+        }
+        public static IEnumerable<TylerFactData> All { private set; get; }
+        public string TheFact { get; set; }
+        public string ShortFact { get; set; }
+        static TylerFactData()
+        {
+            List<TylerFactData> all = new List<TylerFactData>();
+            all.Add(new TylerFactData() { TheFact = "Tyler has visited Costa Rica on a school trip.", ShortFact = "Central America" });
+            all.Add(new TylerFactData() { TheFact = "Tyler is the shortest member of his immediate family.", ShortFact = "Short Fact" });
+            all.Add(new TylerFactData() { TheFact = "Tyler has lived in four different states.", ShortFact = "Homes" });
+            all.Add(new TylerFactData() { TheFact = "Tyler's favorite color is purple and his favorite animal is the giraffe.", ShortFact = "Favorites" });
+            all.Add(new TylerFactData() { TheFact = "Tyler has marched in a marching band at Disney's Epcot in Florida.", ShortFact = "Marching Band" });
+            All = all;
+        }
+    }
+}
